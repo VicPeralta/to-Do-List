9b565a5532bd573a9e8376fd20234507573f923e
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-02-14 21:43:27 -0600

    Set basic HTML structure, styling and js code to generate dynamically the task cards
    deleted:    dist/85ba630cf0c5f29ae3e3.svg
    modified:   dist/index.html
    modified:   dist/main.js
    new file:   src/assets/icons.svg
    new file:   src/assets/noun-three-dot-4287657.svg
    modified:   src/index.html
    modified:   src/index.js
    modified:   src/style.css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './style.css';
+
+const tasksList = [
+  { index: 0, completed: false, description: 'do laundry 0' },
+  { index: 1, completed: false, description: 'do laundry 1' },
+  { index: 2, completed: false, description: 'do laundry 2' },
+  { index: 3, completed: false, description: 'do laundry 3' },
+  { index: 4, completed: false, description: 'do laundry 4' },
+];
+
+function createTaskCards() {
+  const taskContainer = document.querySelector('.task-container');
+  const taskTemplate = document.getElementById('task-template');
+  tasksList.forEach((task) => {
+    const taskCard = taskTemplate.content.cloneNode(true).children[0];
+    taskCard.querySelector('.check').setAttribute('checked', task.completed);
+    taskCard.querySelector('.task-description').value = task.description;
+    taskCard.querySelector('.row-task button').setAttribute('data_id', task.index);
+    taskContainer.appendChild(taskCard);
+  });
+}
+
+createTaskCards();
+const tasks = document.querySelectorAll('.task-description');
+tasks.forEach((task) => {
+  task.addEventListener('input', (e) => {
+    const grandParent = e.target.parentNode.parentNode;
+    grandParent.style.backgroundColor = 'lightgoldenrodyellow';
+    grandParent.querySelector('button').innerHTML = '&#128465;';
+  });
+  task.addEventListener('blur', (e) => {
+    const grandParent = e.target.parentNode.parentNode;
+    grandParent.style.backgroundColor = 'inherit';
+  });
+});
