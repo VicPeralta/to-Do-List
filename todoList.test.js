0a674f98ecd1ca497a7821bc37b76922efbb7b1a
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-23 11:25:29 -0500

    Add Test for HTML Population

--- todoList.test.js
+++ todoList.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import TaskList from './src/tasksList.js';
+import App from './src/app.js';
 
 describe('Tasklist localstorage', () => {
   test('Add', () => {
@@ -21,5 +22,27 @@ describe('Tasklist localstorage', () => {
 
 describe('DOM manipulation', () => {
   test('Add taskCard', () => {
+    document.body.innerHTML = `
+       <template id="task-template" >
+      <div class="row row-task" draggable="true">
+        <div class="task">
+          <input type="checkbox" class="check">
+          <input class="task-description" type="text">
+        </div>
+        <button class='move-btn' type="button">&#8942;</button>
+        <button class='delete-btn hide' type="button">&#128465;</button>
+      </div>
+    </template>
+    <div class="task-container">
+    </div>
+    `;
+    const app = new App();
+    app.taskList.addNewTask('Task 1');
+    app.taskList.addNewTask('Task 2');
+    app.taskList.addNewTask('Task 3');
+    app.taskList.addNewTask('Task 4');
+    app.displayTaskCards();
+    const tasks = document.body.querySelectorAll('.row-task')
+    expect(tasks.length).toBe(4);
   });
 });
